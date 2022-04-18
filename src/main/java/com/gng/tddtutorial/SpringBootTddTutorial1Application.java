d7b94242a1ee63e72869d36c9d892e80d943d0cb
Rurien1124 <a01092603909@gmail.com>
2022-04-18 15:29:13 +0900

    최초 등록

---
+++ src/main/java/com/gng/tddtutorial/SpringBootTddTutorial1Application.java
@@ -0,0 +1,13 @@
+package com.gng.tddtutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTddTutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTddTutorial1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/gng/tddtutorial/SpringBootTddTutorial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gng.tddtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTddTutorial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
